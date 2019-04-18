4f5426d96dff57f28dbad4f1ad0b2e62f401e061
ronserruya <ron.serruya@kik.com>
2019-04-18 11:07:43 +0300

    Correct project stracture

--- bootstrap/src/init.py
+++ bootstrap/src/init.py
@@ -1,49 +1,30 @@
 """Initialization for the bootstrap server"""
-import inspect
 import logging
 
-import kin
-from kin.utils import create_channels
 from sanic import Sanic
 
-from . import log
-from .config import config
+from .log import init_logging
+from .config import Settings
+from .routes import init_routes
+from .middlewares import init_middlewares
 
 VERSION = '1.0.0'
 
-# Setup logging
-log.init_logging()
-logger = logging.getLogger('bootstrap')
-logger.setLevel(config.LOG_LEVEL)
 
-# Setup app-related stuff
-app = Sanic(__name__)
+def init_app(config: Settings) -> Sanic:
+    # Setup logging
+    init_logging()
+    logger = logging.getLogger('bootstrap')
+    logger.setLevel(config.LOG_LEVEL)
 
-# Setup kin-related stuff
-@app.listener('before_server_start')
-async def setup_kin(app, loop):
-    kin_env = kin.Environment('CUSTOM', config.HORIZON_ENDPOINT, config.NETWORK_PASSPHRASE)
-    app.kin_client = kin.KinClient(kin_env)
+    app = Sanic(__name__)
 
-    app.minimum_fee = await app.kin_client.get_minimum_fee()
+    init_routes(app, VERSION)
+    init_middlewares(app, config)
 
-    # Setup channels, skip if we are running in unittests
-    if config.CHANNEL_COUNT > 0 and 'pytest_fixture_setup' not in [a.function for a in inspect.stack()]:
-        logger.info(f'Setting up {config.CHANNEL_COUNT} channels')
-        channels = await create_channels(config.SEED,
-                                         kin_env,
-                                         config.CHANNEL_COUNT,
-                                         config.CHANNEL_STARTING_BALANCE,
-                                         config.CHANNEL_SALT)
-    else:
-        channels = None
-    app.kin_account = app.kin_client.kin_account(config.SEED,
-                                                 channel_secret_keys=channels,
-                                                 app_id=config.APP_ID)
+    # Modify this so that we dont print the full seed to the log
+    printable_config = config.json(indent=2)
+    logger.info(f'Starting with the following config:\n'
+                f'{printable_config.replace(config.SEED, config.SEED[:6] + "*" * 48)}')
 
-
-# Modify this so that we dont print the full seed to the log
-printable_config = config.json(indent=2)
-logger.info(f'Starting with the following config:\n'
-            f'{printable_config.replace(config.SEED, config.SEED[:6] + "*" * 48)}')
-
+    return app
